9f39986101ebbf92c8b7cdedfb300945fe3539f7
binghuojxj <binghuojxj@qq.com>
2019-01-22 11:14:37 +0800

    新增Asp.Net Core创建基本MVC项目以及依赖注入解析配置文件

---
+++ Sample1/Sample1/Controllers/ContentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sample1.Models;
+
+namespace Sample1.Controllers
+{
+    /// <summary>
+    /// 2019-01-22
+    /// JXJ
+    /// 控制器
+    /// </summary>
+    public class ContentController : Controller
+    {
+        private readonly Content contents;
+        public ContentController(Microsoft.Extensions.Options.IOptions<Content> option)
+        {
+            contents = option.Value;
+        }
+        /// <summary>
+        /// 首页显示
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View(new ContentViewModel { Contents = new List<Content> { contents } });
+        }
+    }
+}
